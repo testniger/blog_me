f8553aa7286fe66cb518f8e2120e9b0d9ec1544c
Your Name <evg.urazovskii@gmail.com>
2017-05-17 17:11:45 +0300

    add title

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -28,7 +28,7 @@ AppAsset::register($this);
 <div class="wrap">
     <?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
+        'brandLabel' => 'Niger blog',
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
             'class' => 'navbar-inverse navbar-fixed-top',
@@ -71,7 +71,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="pull-left">&copy; Niger blog <?= date('Y') ?></p>
 
         <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,7 +2,7 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'My Yii Application';
+$this->title = 'Мой тестовый сайт на yii2';
 ?>
 <div class="site-index">
 
